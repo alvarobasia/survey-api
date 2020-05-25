cc5998cc76448bbe2abdee8c49db91d46ce9c3df
Alvaro Basilio de Araujo <alvarobasilioedmar@gmail.com>
2020-05-25 09:56:03 -0300

    test: ensure SingUpController returns 400 if no email passed

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -14,4 +14,17 @@ describe('SingUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing name'))
   })
+  test('Should return 400 if no email passed', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'random',
+        password: 'random',
+        passwordConfirmed: 'random'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing email'))
+  })
 })
