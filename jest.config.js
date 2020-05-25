0cb1854d8cbbc9cb902e09e165bf770c6e2b860b
Alvaro Basilio de Araujo <alvarobasilioedmar@gmail.com>
2020-05-25 08:54:23 -0300

    refactor: eslint jest.config.js rules

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  roots : ['<rootDir>/src'],
-  collectCoverageFrom : ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  transform : {
-    '.*\\.ts$' : 'ts-jest'
-  }
-};
+  transform: { '.*\\.ts$': 'ts-jest' }
+}
