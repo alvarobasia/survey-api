659d78baa8b3aa0ed9e37ae9942da189261e3fbe
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2020-12-27 16:09:50 -0300

    test: ensure BcryptAdapter returns a hash

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adpter'
 
+jest.mock('bcrypt', () => ({
+  hash: async (): Promise<string> => {
+    return Promise.resolve('hash')
+  }
+}))
+
 const SALT = 12
 
 describe('Bcrypt adapter', () => {
@@ -9,5 +15,10 @@ describe('Bcrypt adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_pass')
     expect(hashSpy).toHaveBeenCalledWith('any_pass', SALT)
+  })
+  test('Should return hash', async () => {
+    const sut = new BcryptAdapter(SALT)
+    const hash = await sut.encrypt('any_pass')
+    expect(hash).toBe('hash')
   })
 })
